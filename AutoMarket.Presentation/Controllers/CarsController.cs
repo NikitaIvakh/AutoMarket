6aaf061b6f5e418772ba50e8817a40b897ca4340
Nikita Ivakh <maffy145rus@mail.ru>
2023-07-24 17:17:57 +0300

    Create controllers and HTML templates

---
+++ AutoMarket.Presentation/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMarket.Presentation.Data.Interfaces;
+using AutoMarket.Presentation.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMarket.Presentation.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly ICars _cars;
+        private readonly ICarsCategory _carsCategory;
+
+        public CarsController(ICars cars, ICarsCategory carsCategory)
+        {
+            _cars = cars;
+            _carsCategory = carsCategory;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Page with cars";
+            CarsListViewModel obj = new()
+            {
+                AllCars = _cars.Cars,
+                CurrCategory = "Cars"
+            };
+
+            return View(obj);
+        }
+    }
+}
---
+++ AutoMarket.Presentation/ViewModels/CarsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AutoMarket.Presentation.Data.Models;
+
+namespace AutoMarket.Presentation.ViewModels
+{
+    public class CarsListViewModel
+    {
+        public IEnumerable<Car> AllCars { get; set; }
+
+        public string CurrCategory { get; set; }
+    }
+}
