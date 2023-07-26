f34d90b6d4fa6c0a483f44a0f50442ea31527f7d
Nikita Ivakh <maffy145rus@mail.ru>
2023-07-26 11:27:10 +0300

    The main page of the site

---
+++ AutoMarket.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMarket.Presentation.Data.Interfaces;
+using AutoMarket.Presentation.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMarket.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICars _cars;
+
+        public HomeController(ICars cars)
+        {
+            _cars = cars;
+        }
+
+        public ViewResult Index()
+        {
+            HomeViewModel viewModel = new()
+            {
+                FavoriteCars = _cars.GetFavoriteCars,
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ AutoMarket.Presentation/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AutoMarket.Presentation.Data.Models;
+
+namespace AutoMarket.Presentation.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> FavoriteCars { get; set; }
+    }
+}
